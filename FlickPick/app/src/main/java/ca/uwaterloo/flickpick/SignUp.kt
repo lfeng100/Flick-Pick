01e4f2843dbd0772dc267ef8d7854399af56a96f
ag3722 <90345401+ag3722@users.noreply.github.com>
2025-02-12 13:17:21 -0500

    Login&Signup
    - Login Page
    - SignUp Page

--- FlickPick/app/src/main/java/ca/uwaterloo/flickpick/SignUp.kt
+++ FlickPick/app/src/main/java/ca/uwaterloo/flickpick/SignUp.kt
@@ -1,6 +1,5 @@
 package ca.uwaterloo.flickpick
 
-
 import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
@@ -31,18 +30,16 @@ import androidx.compose.ui.unit.dp
 import ca.uwaterloo.flickpick.ui.theme.FlickPickTheme
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import ca.uwaterloo.flickpick.ui.theme.Purple40
 
 
-class MainActivity : ComponentActivity() {
+class SignUp : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -51,18 +48,19 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Login()
+                    Signup()
                 }
             }
         }
-   }
+    }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun Login() {
+fun Signup() {
     var email = remember { mutableStateOf("") }
     var password = remember { mutableStateOf("") }
+    var confirmPassword = remember { mutableStateOf("") }
 
     Box(
         modifier = Modifier.fillMaxSize()
@@ -76,7 +74,7 @@ fun Login() {
                     .height(305.dp),
             ) {
                 Image(
-                    painter = painterResource(id = ca.uwaterloo.flickpick.R.drawable.login_header_image),
+                    painter = painterResource(id = ca.uwaterloo.flickpick.R.drawable.signup_header_image),
                     contentDescription = "Login Header Image",
                     modifier = Modifier
                         .fillMaxWidth()
@@ -97,10 +95,9 @@ fun Login() {
                         .padding(start = 48.dp, end = 48.dp),
                     horizontalAlignment = Alignment.Start,
                 ) {
-
                     Spacer(modifier = Modifier.height(20.dp))
                     Text(
-                        text = "Login",
+                        text = "Signup",
                         fontSize = 42.sp,
                         //fontWeight = FontWeight.Bold,
                         color = Color.White,
@@ -147,13 +144,25 @@ fun Login() {
                     )
                     Spacer(modifier = Modifier.height(12.dp))
                     Text(
-                        text = "Forgot Password?",
-                        fontSize = 14.sp,
+                        text = "Confirm Password",
+                        fontSize = 17.sp,
                         color = Color.Gray,
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Right
                     )
-                    Spacer(modifier = Modifier.height(12.dp))
+                    Spacer(modifier = Modifier.height(10.dp))
+                    OutlinedTextField(
+                        value = confirmPassword.value,
+                        onValueChange = { confirmPassword.value = it },
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(42.dp),
+                        singleLine = true,
+                        shape = RoundedCornerShape(50.dp),
+                        colors = OutlinedTextFieldDefaults.colors(
+                            focusedBorderColor = Color.Gray,
+                            unfocusedBorderColor = Color.Gray,
+                        )
+                    )
+                    Spacer(modifier = Modifier.height(30.dp))
                     Button(
                         onClick = { },
                         modifier = Modifier
@@ -163,35 +172,8 @@ fun Login() {
                         colors = ButtonDefaults.buttonColors(containerColor = Purple40)
                     ) {
                         Text(
-                            text = "Sign in",
+                            text = "Sign up",
                             fontSize = 20.sp,
-                            //fontWeight = FontWeight.Bold,
-                            color = Color.White,
-                            modifier = Modifier.fillMaxWidth(),
-                            textAlign = TextAlign.Center
-                        )
-                    }
-                    Spacer(modifier = Modifier.height(10.dp))
-                    Text(
-                        text = "or",
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White,
-                        modifier = Modifier.fillMaxWidth(),
-                        textAlign = TextAlign.Center
-                    )
-                    Spacer(modifier = Modifier.height(10.dp))
-                    OutlinedButton(
-                        onClick = { },
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .height(42.dp),
-                        shape = RoundedCornerShape(50.dp),
-                        colors = ButtonDefaults.buttonColors(containerColor = Color.DarkGray),
-                    ) {
-                        Text(
-                            text = "Login with Google",
-                            fontSize = 18.sp,
                             //fontWeight = FontWeight.Bold,
                             color = Color.White,
                             modifier = Modifier.fillMaxWidth(),
@@ -200,7 +182,7 @@ fun Login() {
                     }
                     Spacer(modifier = Modifier.height(65.dp))
                     Text(
-                        text = "Don't Have an Account?",
+                        text = "Already Have an Account?",
                         fontSize = 14.sp,
                         color = Color.Gray,
                         modifier = Modifier.fillMaxWidth(),
@@ -208,7 +190,7 @@ fun Login() {
                     )
                     Spacer(modifier = Modifier.height(2.dp))
                     Text(
-                        text = "Signup",
+                        text = "Login",
                         fontSize = 14.sp,
                         color = Purple40,
                         modifier = Modifier.fillMaxWidth(),
@@ -222,8 +204,8 @@ fun Login() {
 
 @Preview(showBackground = true)
 @Composable
-fun LoginPreview() {
+fun SignupPreview() {
     FlickPickTheme {
-        Login()
+        Signup()
     }
 }
