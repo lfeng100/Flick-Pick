e8913a3d9ccd63abe300eda8b5af46a01ed67be9
Alex Yoshida <a3yoshid@uwaterloo.ca>
2025-03-28 18:17:50 -0400

    added scrollable picker, and tag names for laungauges

--- FlickPick/app/src/main/java/ca/uwaterloo/flickpick/dataObjects/Database/Models/Tag.kt
+++ FlickPick/app/src/main/java/ca/uwaterloo/flickpick/dataObjects/Database/Models/Tag.kt
@@ -7,4 +7,27 @@ data class Tag(
     val tagID: String,
     val tagName: String,
     val tagType: String
-)
+){
+    private val languageNameMap = mapOf(
+        "ar" to "Arabic", "bn" to "Bengali", "cn" to "Chinese (Simplified)",
+        "cs" to "Czech", "da" to "Danish", "de" to "German", "el" to "Greek",
+        "en" to "English", "es" to "Spanish", "et" to "Estonian", "fa" to "Persian",
+        "fi" to "Finnish", "fr" to "French", "ga" to "Irish", "he" to "Hebrew",
+        "hi" to "Hindi", "hu" to "Hungarian", "hy" to "Armenian", "id" to "Indonesian",
+        "is" to "Icelandic", "it" to "Italian", "ja" to "Japanese", "ka" to "Georgian",
+        "ko" to "Korean", "lt" to "Lithuanian", "mk" to "Macedonian", "nl" to "Dutch",
+        "no" to "Norwegian", "pl" to "Polish", "pt" to "Portuguese", "ro" to "Romanian",
+        "ru" to "Russian", "sr" to "Serbian", "sv" to "Swedish", "sw" to "Swahili",
+        "ta" to "Tamil", "te" to "Telugu", "th" to "Thai", "tl" to "Tagalog",
+        "tr" to "Turkish", "uk" to "Ukrainian", "ur" to "Urdu", "wo" to "Wolof",
+        "xx" to "No Language", "zh" to "Chinese (Traditional)"
+    )
+
+    fun displayName(): String {
+        return if (tagType == "language"){
+            languageNameMap[tagID] ?: tagName
+        }else{
+            tagName
+        }
+    }
+}
